cf8c8a763ccef82a57ea54f7313993ce5381286d
Niklavs Bergs <niklavs.bergs@magebit.com>
2024-04-02 10:34:36 +0300

    Added weather api endpoints

---
+++ src/api/extensions/weather-api/index.js
@@ -0,0 +1,63 @@
+import { Router } from 'express';
+import request from 'request';
+
+module.exports = ({ config }) => {
+  const api = Router();
+
+  api.get('/day', (req, res) => {
+    const { date } = req.query;
+    const { accessToken } = config.extensions.weather;
+    var url;
+
+    if (!date) {
+      url = `http://api.weatherapi.com/v1/current.json?key=${accessToken}&q=Riga&aqi=no`;
+    } else {
+      url = `http://api.weatherapi.com/v1/future.json?key=${accessToken}&q=Riga&dt=${date}`;
+    }
+
+    request(
+      {
+        url
+      },
+      (error, response, body) => {
+        let apiResult;
+        const errorResponse = error || body.error;
+
+        if (errorResponse) {
+          apiResult = { code: 500, result: errorResponse };
+        } else {
+          const responseBody = JSON.parse(body);
+          apiResult = { code: 200, result: responseBody };
+        }
+
+        res.status(apiResult.code).json(apiResult);
+      }
+    );
+  });
+
+  api.get('/forecast', (req, res) => {
+    const { accessToken } = config.extensions.weather;
+    const url = `http://api.weatherapi.com/v1/forecast.json?key=${accessToken}&q=Riga&days=3&aqi=no&alerts=no`;
+
+    request(
+      {
+        url
+      },
+      (error, response, body) => {
+        let apiResult;
+        const errorResponse = error || body.error;
+
+        if (errorResponse) {
+          apiResult = { code: 500, result: errorResponse };
+        } else {
+          const responseBody = JSON.parse(body);
+          apiResult = { code: 200, result: responseBody };
+        }
+
+        res.status(apiResult.code).json(apiResult);
+      }
+    );
+  });
+
+  return api;
+};
